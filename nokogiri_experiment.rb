6c14cb548eb7bad6c08334bf9d0c3adc2dc74896
Allison <azywong@gmail.com>
2014-05-23 15:33:12 -0700

    added nokogiri experiment file

---
+++ nokogiri_experiment.rb
@@ -0,0 +1,10 @@
+require 'nokogiri'
+require 'open-uri'
+page = Nokogiri::HTML(open('http://www.huffingtonpost.com'))
+puts page.class 	# Nokogiri::HTML::Document
+links = page.css("a") # finding all links
+puts links.class    # Nokogiri::XML::NodeSet
+puts links.length   # number of links
+
+images = page.css("img")  # finding all images
+puts images.length        # number images

64a9c700c67feab8a1949cf69962675b777667ff
C.J. Jameson <cjcjameson@gmail.com>
2014-05-23 15:27:19 -0700

    comment expansion especially html list

--- core.rb
+++ core.rb
@@ -1,15 +1,18 @@
 class WebsiteFetcher
-	def get_console_input()
+	def self.get_console_input()
 		# verify well formed, raise if not
 	end
 
-	def send_to_html_parser
+	def self.send_to_html_parser
+		HTMLParser.kick_to_nokogiri(univ_record_locator)
 	end
 end
 
 class HTMLParser
 	def kick_to_nokogiri
-		#return fat_blob -> is a file (filename?)
+		# return fat_blob -> is a file (filename?)
+		# Nokogiri::HTML::Document?
+		# or just access from the web?
 		# verify nokogiri success
 	end
 
@@ -22,6 +25,20 @@ class Frequency Analyzer
 	def initialize_containers
 		# some kind of storage for data
 		# list of all tags
+			#<a> (link)
+			#<br>
+			#<center>
+			#<div>
+			#<em>
+			#<head>
+			#<h1><h2><...>
+			#<i> italics
+			#<img>
+			#<p>
+			#<span>
+			#<strong>
+			#<table>
+			#<ul><li>
 	end
 	
 	def count_tags
@@ -34,6 +51,6 @@ class Display
 	end
 
 	def js_spit_friendly
-		# not implemented in release 0
+		raise NoMethodError "not implemented in release 0"
 	end
 end
